fa1344f30d10b11a58ef025670259587e8e427e4
Favour <ukasoanya@systemspecs.com.ng>
2023-08-27 23:12:17 +0100

    Fix access error 4

--- src/main/java/stslekiti/receipt/ReceiptApplication.java
+++ src/main/java/stslekiti/receipt/ReceiptApplication.java
@@ -23,29 +23,29 @@ public class ReceiptApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-//		Receipt person1 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
-//				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
-//				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
-//				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
-//		receiptRepository.save(person1);
-//
-//		Receipt person2 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
-//				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
-//				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
-//				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
-//		receiptRepository.save(person2);
-//
-//		Receipt person3 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
-//				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
-//				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
-//				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
-//		receiptRepository.save(person3);
-//
-//		Receipt person4 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
-//				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
-//				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
-//				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
-//		receiptRepository.save(person4);
+		Receipt person1 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
+				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
+				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
+				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
+		receiptRepository.save(person1);
+
+		Receipt person2 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
+				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
+				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
+				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
+		receiptRepository.save(person2);
+
+		Receipt person3 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
+				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
+				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
+				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
+		receiptRepository.save(person3);
+
+		Receipt person4 = Receipt.builder().accessedValue(BigInteger.valueOf(12340000)).amountPreviouslyOwed(BigInteger.valueOf(750))
+				.bankPaymentCode(BigInteger.valueOf(123456789)).chargeRate(0.0394).contactAddress("1 Omobajesu Close, Akowonjo, Lagos State")
+				.lucAmount(BigInteger.valueOf(5735)).noticeNumber(BigInteger.valueOf(1212121212)).noticeDate(LocalDate.now()).printDate(LocalDate.now())
+				.payerId("1-PS").payerName("John Doe").propertyAddress("1 Oniru Estate, Victoria Island, Lagos State").propertyClassification("Occupied").build();
+		receiptRepository.save(person4);
 
 	}
 }
