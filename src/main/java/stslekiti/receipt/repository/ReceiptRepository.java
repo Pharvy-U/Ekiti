75e752db8b6ec95407dba2258269350718f87e16
Favour <ukasoanya@systemspecs.com.ng>
2023-10-11 06:11:59 +0100

    first commit on new branch

--- src/main/java/stslekiti/receipt/repository/ReceiptRepository.java
+++
@@ -1,7 +0,0 @@
-package stslekiti.receipt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import stslekiti.receipt.entity.Receipt;
-
-public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
-}
